83a1f1aba5b587da751915ad96024a21a4265818
OZ <normandiggs@gmail.com>
2013-03-23 00:46:33 +0400

    cosmetic

--- lib/MailRoute/API/EntityHandler.php
+++ lib/MailRoute/API/EntityHandler.php
@@ -13,16 +13,6 @@ class EntityHandler implements IEntity
 		$this->entity_name = $entity_name;
 	}
 
-//	public function __call($function, $arguments)
-//	{
-//		$method           = $function;
-//		$url_request_part = '/'.$this->entity_name.'/';
-//		$this->handleEntityRequest($arguments, $method, $url_request_part);
-//		$result = $this->Client->callAPI($url_request_part, $method, $arguments);
-//		$result = $this->handleAPIResponse($method, $result);
-//		return $result;
-//	}
-
 	protected function callAPI($method, $add_to_query_path, $arguments = array())
 	{
 		$url_request_part = '/'.$this->entity_name.'/'.$add_to_query_path;
@@ -51,50 +41,6 @@ class EntityHandler implements IEntity
 			}
 		}
 		return $result;
-	}
-
-	/**
-	 * @param $arguments
-	 * @param $method
-	 * @param $url_request_part
-	 */
-	protected function handleEntityRequest(&$arguments, &$method, &$url_request_part)
-	{
-		if ($method=='GET' && !empty($arguments))
-		{
-			$id = array_shift($arguments);
-			if (!empty($id))
-			{
-				$url_request_part .= $id.'/';
-			}
-			if (empty($arguments)) return;
-			$filters = array_shift($arguments);
-			if (!empty($arguments))
-			{
-				$offset = array_shift($arguments);
-			}
-			if (!empty($arguments))
-			{
-				$limit = array_shift($arguments);
-			}
-			$arguments = $filters;
-			if (!empty($offset))
-			{
-				$arguments['offset'] = $offset;
-			}
-			if (!empty($limit))
-			{
-				$arguments['limit'] = $limit;
-			}
-		}
-		elseif ($method=='PUT')
-		{
-			$id = $arguments[0]['id'];
-			if (!empty($id))
-			{
-				$url_request_part .= $id.'/';
-			}
-		}
 	}
 
 	public function setEntityName($entity_name)
